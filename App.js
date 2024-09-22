0a17213f78916adae84b193acd1f7dc5ce23b9d4
serendiipity <imane.chk@gmail.com>
2024-09-22 01:57:47 +0100

    add skeleton host code

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View , TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [hostUserId, setHostUserId] = useState('');
+  const [roomCode, setRoomCode] = useState('');
+  const [isHost, setIsHost] = useState(false);
+
+  const createRoom = async () => {
+
+  };
+
+  const joinRoom = async () => {
+
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Musicly Yours</Text>
+      <TextInput
+        style ={styles.input}
+        placeholder="Enter User ID"
+        value={hostUserId}
+        onChangeText={setHoserUserId}
+      />
+      {roomCode ? (
+        <Room roomCode={roomCode} isHost={isHost} hostUserId={hostUserId} />
+      ) : (
+        <View>
+          <Button title="Create Room" onPress={createRoom} />
+          <TextInput
+            style={styles.input}
+            placeholder="Room Code"
+            value={roomCode}
+            onChangeText={setRoomCode}
+          />
+          <Button title="Join Room" onPress={joinRoom} />
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +45,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 16
   },
+  input: {
+    width: '100%',
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 8,
+    marginBottom: 16
+  }
 });
